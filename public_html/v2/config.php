5e874c2776a9975c3f846a4bdc7d477a7f1da05e
Kim Steinhaug <kim@steinhaug.com>
2025-06-05 15:28:21 +0200

    Preparing v2 Map demo

--- public_html/v2/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('MAPBOX_TOKEN', 'pk.eyJ1IjoiYm9zc21hbjc1ODEiLCJhIjoiY2xmb3E5bHQ3MHVpYzN1bWhmaWoyN3h2OSJ9.mPV4SK4sSBApOHp6evb78A');
-define('API_BASE_URL', '/api/');
-define('DEFAULT_CENTER_LNG', 8.0059);
-define('DEFAULT_CENTER_LAT', 58.1467);
-define('DEFAULT_ZOOM', 12);
